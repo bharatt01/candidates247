87f185ab4ebcf351b190ff01678e8cac14847b1f
bharat <bharatsharma7456@gmail.com>
2026-04-09 11:53:51 +0530

    live

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import path from "path";
 import componentTagger from "vite-plugin-component-tagger";
 
 export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: "public_html",  // ⚠️ careful: relative to project root
+  },
+
   base: "/",   // ✅ ADD THIS LINE
   server: {
     host: "::",
